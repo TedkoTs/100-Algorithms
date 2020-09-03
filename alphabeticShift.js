96504e13526bfaa014362f4c3aead02772bd6b7f
Teodor Tsvetanov <teodor.tzvetanov@gmail.com>
2020-09-03 16:40:35 +0300

    small fix

--- alphabeticShift.js
+++ alphabeticShift.js
@@ -3,15 +3,17 @@
 // For inputString = "crazy", the output should be alphabeticShift(inputString) = "dsbaz".
 
 const alphabeticShift = inputString => {
-    let res = inputString.split('').map(el => {
-        if (el === 'z') {
-            return 'a'
-        } else {
-            return String.fromCharCode(el.charCodeAt(0) + 1)
-        }
-    })
 
-    return res.join('')
+    return inputString
+        .split('')
+        .map(el => {
+            if (el === 'z') {
+                return 'a'
+            } else {
+                return String.fromCharCode(el.charCodeAt(0) + 1)
+            }
+        })
+        .join('')
 }
 
 console.log(alphabeticShift('crazy'));
